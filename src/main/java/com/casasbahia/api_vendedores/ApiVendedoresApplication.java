8b5cfd75d133d491d1ff0b5a88f1b145b9f1c746
Sidney Miranda <sidney.miranda2013@gmail.com>
2024-08-17 12:34:57 -0300

    first commit

---
+++ src/main/java/com/casasbahia/api_vendedores/ApiVendedoresApplication.java
@@ -0,0 +1,13 @@
+package com.casasbahia.api_vendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendedoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casasbahia/api_vendedores/ApiVendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casasbahia.api_vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
